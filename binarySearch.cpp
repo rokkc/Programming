a362ecae87fc0b741c2a1dd8f4a6918d02781ea5
Yash Pattekar <109927564+rokkc@users.noreply.github.com>
2024-10-28 17:49:04 -0700

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,1 +1,37 @@
-//hello
+int first_true(int lo, int hi) {
+	hi++;
+	while (lo < hi) {
+		int mid = lo + (hi - lo) / 2;
+		if (f(mid)) {
+			hi = mid;
+		} else {
+			lo = mid + 1;
+		}
+	}
+	return lo;
+}
+
+int last_true(int lo, int hi) {
+	lo--;
+	while (lo < hi) {
+		int mid = lo + (hi - lo + 1) / 2;
+		if (f(mid)) {
+			lo = mid;
+		} else {
+			hi = mid - 1;
+		}
+	}
+	return lo;
+}
+
+pair<int, int> findClosestValues(vector<int>& vec, int target) {
+    int n = vec.size();
+    if (n == 0) return {INT_MIN, INT_MIN};
+    auto right = lower_bound(vec.begin(), vec.end(), target);
+    int rightClosest = (right == vec.end()) ? INT_MIN : *right;
+
+    auto left = (right == vec.begin()) ? vec.end() : prev(right);
+    int leftClosest = (left == vec.end()) ? INT_MIN : *left;
+
+    return {leftClosest, rightClosest};
+}
