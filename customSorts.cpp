6b6fe7580c420523faba858810dfe89db12fe34f
Yash Pattekar <109927564+rokkc@users.noreply.github.com>
2024-11-23 21:50:27 -0800

    Update customSorts.cpp

--- customSorts.cpp
+++ customSorts.cpp
@@ -1,5 +1,3 @@
-bool sortBySecond(const pair<int,int> &a,
-            const pair<int,int> &b)
-{
+bool sortBySecond(const pair<int,int> &a, const pair<int,int> &b) {
     return (a.second < b.second);
 }
