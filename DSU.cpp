0557330af0eda7ac2dbf7d0a2d2e65d39d2489c1
Yash Pattekar <109927564+rokkc@users.noreply.github.com>
2024-12-02 21:55:25 -0800

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,46 +1,57 @@
-vector<int> parent;
-vector<int> rankVec;
+class DSU {
+private:
+    vector<int> parent, rank;
 
-void createDSU(int n) {
-    parent.resize(n);
-    rankVec.resize(n, 0);
-    for (int i = 0; i < n; ++i) {
-        parent[i] = i;
-    }
-}
-
-int find(int u) {
-    if (parent[u] != u) {
-        parent[u] = find(parent[u]);
-    }
-    return parent[u];
-}
-
-void unite(int u, int v) {
-    int rootU = find(u);
-    int rootV = find(v);
-    if (rootU != rootV) {
-        if (rankVec[rootU] > rankVec[rootV]) {
-            parent[rootV] = rootU;
-        } else if (rankVec[rootU] < rankVec[rootV]) {
-            parent[rootU] = rootV;
-        } else {
-            parent[rootV] = rootU;
-            rankVec[rootU]++;
+public:
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
     }
-}
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void unionSets(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
 
 int main() {
-    createDSU(5);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
 
-    unite(0, 2);
-    unite(4, 2);
-    unite(3, 1);
+    DSU dsu(10);
 
-    if (find(1) == find(0)) {
-        cout << "Yes\n";
-    } else {
-        cout << "No\n";
-    }
+    dsu.unionSets(0, 1);
+    dsu.unionSets(1, 2);
+    dsu.unionSets(3, 4);
+
+    cout << "Is 0 and 2 connected? " << (dsu.connected(0, 2) ? "Yes" : "No") << endl;
+    cout << "Is 0 and 4 connected? " << (dsu.connected(0, 4) ? "Yes" : "No") << endl;
 }
