7cdf3dc58b9d46a32864844c042b1d30eb04ec08
Yash Pattekar <109927564+rokkc@users.noreply.github.com>
2025-01-07 19:54:43 -0800

    Create default.cpp

---
+++ default.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+using namespace std;
+#pragma GCC optimize("O3")
+#define int long long
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
+}
