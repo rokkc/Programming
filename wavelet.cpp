0caa8c6afde295effee3dec44145f26ae6fd82bb
Yash Pattekar <109927564+rokkc@users.noreply.github.com>
2025-01-15 20:55:53 -0800

    Update wavelet.cpp

--- wavelet.cpp
+++ wavelet.cpp
@@ -1,12 +1,13 @@
+template <typename T>
 class WaveletTree {
 private:
-    int lo, hi;
+    T lo, hi;
     WaveletTree *l, *r;
-    vector<int> b;
+    std::vector<int> b;
 
 public:
-    WaveletTree(vector<int>::iterator from, vector<int>::iterator to, int x = LLONG_MIN, int y = LLONG_MAX) 
-        : lo(*min_element(from, to)), hi(*max_element(from, to)), l(nullptr), r(nullptr) {
+    WaveletTree(typename std::vector<T>::iterator from, typename std::vector<T>::iterator to, T x = std::numeric_limits<T>::min(), T y = std::numeric_limits<T>::max())
+        : lo(*std::min_element(from, to)), hi(*std::max_element(from, to)), l(nullptr), r(nullptr) {
         if (from >= to) return;
         if (lo == hi) {
             b.reserve(to - from + 1);
@@ -16,19 +17,19 @@ public:
             }
             return;
         }
-        int mid = lo + (hi - lo) / 2;
-        auto f = [mid](int x) { return x <= mid; };
+        T mid = lo + (hi - lo) / 2;
+        auto f = [mid](T x) { return x <= mid; };
         b.reserve(to - from + 1);
         b.push_back(0);
         for (auto it = from; it != to; ++it) {
             b.push_back(b.back() + f(*it));
         }
-        auto pivot = stable_partition(from, to, f);
+        auto pivot = std::stable_partition(from, to, f);
         l = new WaveletTree(from, pivot, lo, mid);
         r = new WaveletTree(pivot, to, mid + 1, hi);
     }
 
-    int kth(int l, int r, int k) {
+    T kth(int l, int r, int k) {
         if (l > r) return -1;
         if (lo == hi) return lo;
         int inLeft = b[r + 1] - b[l];
@@ -39,7 +40,7 @@ public:
         }
     }
 
-    int LTE(int l, int r, int x) {
+    int LTE(int l, int r, T x) {
         if (l > r || x < lo) return 0;
         if (hi <= x) return r - l + 1;
         return this->l->LTE(b[l], b[r + 1] - 1, x) + this->r->LTE(l - b[l], r - b[r + 1], x);
