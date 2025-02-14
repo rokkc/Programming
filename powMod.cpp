dabe64292ed150121232dfd040fd54655d69b934
Yash Pattekar <109927564+rokkc@users.noreply.github.com>
2025-02-14 11:52:58 -0800

    Create powMod.cpp

---
+++ powMod.cpp
@@ -0,0 +1,11 @@
+int powMod(int a, int b, int n){
+    long long x=1, y=a; 
+    while (b > 0) {
+        if (b%2 == 1) {
+            x = (x*y) % n;
+        }
+        y = (y*y) % n;
+        b /= 2;
+    }
+    return x % n;
+}
