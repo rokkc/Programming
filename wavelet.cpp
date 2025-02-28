c8d0de1effee167ffd5d9b921a261b769be54317
Yash Pattekar <109927564+rokkc@users.noreply.github.com>
2025-02-28 10:23:27 -0800

    Update wavelet.cpp

--- wavelet.cpp
+++ wavelet.cpp
@@ -1,48 +1,51 @@
 template <typename T>
 class WaveletTree {
 private:
-    T lo, hi;
+    int lo, hi;
     WaveletTree *l, *r;
-    std::vector<int> b;
-
+    vector<int> b;
+    vector<T>* values;
+    bool isRoot;
 public:
-    WaveletTree(typename std::vector<T>::iterator from, typename std::vector<T>::iterator to, T x = std::numeric_limits<T>::min(), T y = std::numeric_limits<T>::max())
-        : lo(*std::min_element(from, to)), hi(*std::max_element(from, to)), l(nullptr), r(nullptr) {
-        if (from >= to) return;
-        if (lo == hi) {
-            b.reserve(to - from + 1);
-            b.push_back(0);
-            for (auto it = from; it != to; ++it) {
-                b.push_back(b.back() + 1);
-            }
-            return;
-        }
-        T mid = lo + (hi - lo) / 2;
-        auto f = [mid](T x) { return x <= mid; };
-        b.reserve(to - from + 1);
-        b.push_back(0);
-        for (auto it = from; it != to; ++it) {
-            b.push_back(b.back() + f(*it));
-        }
-        auto pivot = std::stable_partition(from, to, f);
-        l = new WaveletTree(from, pivot, lo, mid);
-        r = new WaveletTree(pivot, to, mid + 1, hi);
+    WaveletTree(typename vector<T>::iterator from, typename vector<T>::iterator to) {
+        isRoot = true;
+        vector<T> temp(from, to);
+        vector<T> sorted = temp;
+        sort(sorted.begin(), sorted.end());
+        sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
+        values = new vector<T>(sorted.begin(), sorted.end());
+        lo = 0; hi = values->size() - 1;
+        vector<int> comp; comp.resize(temp.size());
+        for (size_t i = 0; i < temp.size(); ++i) comp[i] = lower_bound(values->begin(), values->end(), temp[i]) - values->begin();
+        b.reserve(comp.size() + 1); b.push_back(0);
+        if (lo == hi) { for (auto x : comp) b.push_back(b.back() + 1); l = r = nullptr; }
+        else { int mid = lo + (hi - lo) / 2; auto f = [mid](int x){ return x <= mid; }; for (auto x : comp) b.push_back(b.back() + (f(x) ? 1 : 0)); auto pivot = stable_partition(comp.begin(), comp.end(), f); l = (comp.begin() != pivot) ? new WaveletTree(comp.begin(), pivot, lo, mid, values) : nullptr; r = (pivot != comp.end()) ? new WaveletTree(pivot, comp.end(), mid + 1, hi, values) : nullptr; }
     }
-
-    T kth(int l, int r, int k) {
-        if (l > r) return -1;
-        if (lo == hi) return lo;
-        int inLeft = b[r + 1] - b[l];
-        if (k < inLeft) {
-            return this->l->kth(b[l], b[r + 1] - 1, k);
-        } else {
-            return this->r->kth(l - b[l], r - b[r + 1], k - inLeft);
-        }
+private:
+    WaveletTree(typename vector<int>::iterator from, typename vector<int>::iterator to, int lo, int hi, vector<T>* values)
+         : lo(lo), hi(hi), l(nullptr), r(nullptr), values(values), isRoot(false) {
+        b.reserve((to - from) + 1); b.push_back(0);
+        if (lo == hi) { for (auto it = from; it != to; ++it) b.push_back(b.back() + 1); return; }
+        int mid = lo + (hi - lo) / 2; auto f = [mid](int x){ return x <= mid; };
+        for (auto it = from; it != to; ++it) b.push_back(b.back() + (f(*it) ? 1 : 0));
+        auto pivot = stable_partition(from, to, f);
+        if (from != pivot) l = new WaveletTree(from, pivot, lo, mid, values);
+        if (pivot != to) r = new WaveletTree(pivot, to, mid + 1, hi, values);
     }
-
-    int LTE(int l, int r, T x) {
-        if (l > r || x < lo) return 0;
-        if (hi <= x) return r - l + 1;
-        return this->l->LTE(b[l], b[r + 1] - 1, x) + this->r->LTE(l - b[l], r - b[r + 1], x);
+public:
+    T kth(int i, int j, int k) {
+        if (i > j) return T();
+        if (lo == hi) return (*values)[lo];
+        int inLeft = b[j + 1] - b[i];
+        if (k < inLeft) return l->kth(b[i], b[j + 1] - 1, k);
+        else return r->kth(i - b[i], j - b[j + 1], k - inLeft);
     }
+    int LTE(int i, int j, T x) {
+        if (i > j || x < (*values)[lo]) return 0;
+        if ((*values)[hi] <= x) return j - i + 1;
+        int leftCount = (l ? l->LTE(b[i], b[j + 1] - 1, x) : 0);
+        int rightCount = (r ? r->LTE(i - b[i], j - b[j + 1], x) : 0);
+        return leftCount + rightCount;
+    }
+    ~WaveletTree() { delete l; delete r; if (isRoot) delete values; }
 };
